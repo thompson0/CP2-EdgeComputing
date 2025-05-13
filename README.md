7ded44acf487f584f2dcf5d589c11cbdf84a4484
thompson0 <thompsonjooj@gmail.com>
2025-05-13 14:24:18 -0300

    codigo e readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# CP2-edgeComputing-
+# Vinheria Agnello - Monitoramento de Ambiente com Arduino
+
+Projeto desenvolvido para o Checkpoint 02 da disciplina Edge Computing & Computer Systems no curso de Engenharia de Software - FIAP.
+
+## Integrantes
+
+-Gabriel Thompson
+-Nicolas Baradel
+-João Pedro Sassarão
+-Juan Pablo Molina
+
+## Objetivo
+
+Atender à demanda da Vinheria Agnello de monitorar as condições ambientais do depósito (luminosidade, temperatura e umidade), garantindo a qualidade dos vinhos armazenados, com alertas visuais e sonoros e exibição das informações em display LCD.
+
+## Tecnologias e Componentes Utilizados
+
+- Arduino UNO
+- Sensor DHT22 (Temperatura e Umidade)
+- Fotoresistor (LDR) para luminosidade
+- Display LCD 16x2 com I2C
+- LEDs: Vermelho, Amarelo e Verde
+- Buzzer
+- IDE Arduino
+
+## Funcionalidades Implementadas
+
+- Leitura da luminosidade e acionamento dos LEDs conforme o nível:
+
+- Leitura da temperatura com sensor DHT22 e exibição:
+
+- Leitura da umidade com sensor DHT22 e exibição:
+
+- Exibição de mensagens no display LCD para todas as condições acima:
+
+## Foto do Prototipo
+![](https://github.com/user-attachments/assets/a86f02c5-1a20-4f47-b501-5ef486022c43)
+
+[- Simulação disponível no Wokwi](https://wokwi.com/projects/428143948516640769)
+- Código-fonte disponível neste repositório no arquivo `sketch.ino`
+
+
+
+## Link do video explicativo
---
+++ sketch.ino
@@ -0,0 +1,251 @@
+int ledRedPin = 13;
+int ledYellowPin = 12;
+int ledGreenPin = 11;
+int ldrPhotoSensor = A0;
+int boozerPin = 8;
+unsigned long boozerStartTime = 0;
+bool boozerActive = false;
+float ultimasTemperaturas[5] = {0};
+int indiceTemp = 0;
+float ultimasUmidades[5]= {0};
+int indiceUmi = 0;
+bool mediaCalculada = false;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#define col 16
+#define lin  2
+#define ende  0x27
+LiquidCrystal_I2C lcd(ende, col, lin);
+
+
+byte shit[8] = {
+  B00001,
+  B00011,
+  B00101,
+  B01001,
+  B10001,
+  B10001,
+  B11111,
+};
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTTYPE DHT22
+#define DHTPIN 2
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+
+void setup() {
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledYellowPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(boozerPin, OUTPUT);
+  Serial.begin(115200);
+  dht.begin();
+
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(sensor.name);
+  Serial.print(sensor.max_value);
+  Serial.print(sensor.min_value);
+  Serial.print(sensor.resolution);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Display Testing!");
+  lcd.setCursor(0, 1);
+  lcd.print("Arduino LCD");
+  lcd.createChar(0, shit);
+  lcd.setCursor(15, 1);  
+  lcd.write(byte(0));
+  dht.humidity().getSensor(&sensor);
+  Serial.println(sensor.name);
+  Serial.print(sensor.max_value);
+  Serial.print(sensor.min_value);
+  Serial.print(sensor.resolution);
+  delayMS = sensor.min_delay / 1000;
+  delay(1000);
+}
+
+void loop() {
+  
+  int ldrValue = analogRead(ldrPhotoSensor);
+  Serial.println(ldrPhotoSensor);
+  unsigned long currentTime = millis();
+
+  digitalWrite(ledGreenPin, LOW);
+  digitalWrite(ledYellowPin, LOW);
+  digitalWrite(ledRedPin, LOW);
+
+  if (ldrValue >= 0 && ldrValue <= 300) {
+    digitalWrite(ledGreenPin, HIGH);
+    noTone(boozerPin);
+    boozerActive = false;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Luz boa");
+    delay(300);
+    lcd.clear();
+  }
+  else if (ldrValue >= 301 && ldrValue <= 800) {
+    digitalWrite(ledYellowPin, HIGH);
+    noTone(boozerPin);
+    boozerActive = false;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Ambiente a meia luz");
+    delay(300);
+    lcd.clear();
+  }
+  else if (ldrValue > 801) {
+    digitalWrite(ledRedPin, HIGH);
+    if (!boozerActive) {
+      tone(boozerPin, 262, currentTime);
+      boozerStartTime = currentTime;
+      boozerActive = true;
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Ambiente muito claro");
+    delay(3000);
+    lcd.clear();
+  }
+  else {
+    noTone(boozerPin);
+    boozerActive = false;
+  }
+
+  //DHT
+  sensors_event_t tempEvent, humidEvent;
+  dht.temperature().getEvent(&tempEvent);
+  dht.humidity().getEvent(&humidEvent);
+
+    if (isnan(tempEvent.temperature)) {
+    Serial.println("Erro na leitura da Temperatura!");
+  }
+
+    else {
+    Serial.print(tempEvent.temperature);
+    Serial.println(" Temperatura em C ");
+    
+  }
+      if (isnan(humidEvent.relative_humidity)) {
+    Serial.println("Erro na leitura da Umidade!");
+  } 
+    else {
+    Serial.print(humidEvent.relative_humidity);
+    Serial.println(" Umidade em % ");
+    
+    }
+
+  ultimasTemperaturas[indiceTemp] = tempEvent.temperature;
+  indiceTemp = (indiceTemp + 1) %5 ;
+  float mediaTemperatura = (
+  ultimasTemperaturas[0] + ultimasTemperaturas[1] + ultimasTemperaturas[2] + ultimasTemperaturas[3] + ultimasTemperaturas[4]
+  ) / 5.0;
+  ultimasUmidades[indiceUmi] = humidEvent.relative_humidity;
+  indiceUmi = (indiceUmi + 1) %5;
+  float mediaUmi = (
+  ultimasUmidades[0] + ultimasUmidades[1] + ultimasUmidades[2] + ultimasUmidades[3] + ultimasUmidades[4]
+  ) / 5.0;
+  if (indiceTemp == 0 && indiceUmi == 0) {
+  mediaCalculada = true;
+  }
+
+  if(mediaCalculada){
+      //temperatura
+    if (mediaTemperatura >= 10 && mediaTemperatura <= 15) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp. OK: ");
+      lcd.setCursor(0,1);
+      lcd.print("Temp. = ");
+      lcd.print(mediaTemperatura);
+      lcd.print("C");
+      delay(3000);
+      lcd.clear();
+    }
+    else if(mediaTemperatura > 15) {
+      digitalWrite(ledYellowPin, HIGH);
+      if (!boozerActive) {
+        tone(boozerPin, 262, currentTime);
+        boozerStartTime = currentTime;
+        boozerActive = true;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp. Alta: ");
+      lcd.setCursor(0,1);
+      lcd.print("Temp. = ");
+      lcd.print(mediaTemperatura);
+      lcd.print("C");
+      delay(3000);
+      lcd.clear();
+    }
+  else if(mediaTemperatura < 10) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp. Baixa: ");
+      lcd.setCursor(0,1);
+      lcd.print("Temp. = ");
+      lcd.print(mediaTemperatura);
+      lcd.print("C");
+      delay(3000);
+      lcd.clear();
+    }
+
+    //umidade
+
+    if (mediaUmi >= 50 && mediaUmi <= 70) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Umidade OK: ");
+      lcd.setCursor(0,1);
+      lcd.print("Umidade = ");
+      lcd.print(mediaUmi);
+      lcd.print("%");
+      delay(3000);
+      lcd.clear();
+    }
+    else if (mediaUmi > 70) {
+      digitalWrite(ledRedPin, HIGH);
+      if (!boozerActive) {
+        tone(boozerPin, 262, currentTime);
+        boozerStartTime = currentTime;
+        boozerActive = true;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Umidade Alta: ");
+      lcd.setCursor(0,1);
+      lcd.print("Umidade = ");
+      lcd.print(mediaUmi);
+      lcd.print("%");
+      delay(3000);
+      lcd.clear();
+    }
+    else if (mediaUmi < 50) {
+      digitalWrite(ledRedPin, HIGH);
+      if (!boozerActive) {
+        tone(boozerPin, 262, currentTime);
+        boozerStartTime = currentTime;
+        boozerActive = true;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Umidade Baixa: ");
+      lcd.setCursor(0,1);
+      lcd.print("Umidade = ");
+      lcd.print(mediaUmi);
+      lcd.print("%");
+      delay(3000);
+      lcd.clear();
+    }
+  delay(200);
+
+  }
+}
